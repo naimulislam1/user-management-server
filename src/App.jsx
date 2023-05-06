ea9e6bc31b21f1aa9f77a4cf6980c16c1a4fa236
naimulislam1 <hpy.man.3232@gmail.com>
2023-05-06 14:04:00 +0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        form.reset();
+      });
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>User Management System</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1>user : {users.length}</h1>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="name" id="" />
+            <br />
+            <input type="email" name="email" id="" />
+            <br />
+            <input type="submit" value="Add" />
+          </form>
+          {users?.map((user) => (
+            <p key={user.id}>
+              {user.id}. {user.name} : {user.email}
+            </p>
+          ))}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
